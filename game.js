4edec86383c4e59f213e8a7e2cf652faa9d125ec
DennisMWarner <warner.dennis.m@gmail.com>
2020-03-20 16:44:58 -0600

    some functionality added to buttons, updated html

--- game.js
+++ game.js
@@ -1,5 +1,73 @@
-console.log("game.js linked....");
+//console.log("game.js linked....");
+let totalPts = 0;
+let activeModRate = 0;
+let basePoints = 1;
+let donkeys = 0;
 
-function test() {
-  console.log("Moon clicked");
+let player = {
+  level: "",
+  nextLevel: 0,
+  donkey: false,
+  activeMods: []
+};
+
+let mods = {
+  moonPickaxe: { mod: 2, cost: 100, costMultiplier: 2 },
+  moonDonkey: { mod: 4, cost: 1000, costMultiplier: 2 },
+  moonSaddlebag: { mod: 3, cost: 200, costMultiplier: 2 },
+  moonWagon: { mod: 5, cost: 5000, costMultiplier: 2 },
+  moonCarrots: { mod: 1, cost: 200, costMultplier: 1 }
+};
+
+function carrotGenerator() {}
+function clickedMoon() {
+  //console.log("Moon clicked");
+  totalPts++;
+  drawAll();
+}
+function clickedMod1(modKey) {
+  let modValue = mods[modKey].mod;
+  player.activeMods.push(modValue);
+  console.log(player.activeMods);
+  //console.log("Mod 1 clicked");
+}
+function clickedMod2(modKey) {
+  let modValue = mods[modKey].mod;
+  player.activeMods.push(modValue);
+  console.log(player.activeMods);
+  //console.log("Mod 2 clicked");
+}
+function clickedMod3(modKey) {
+  let modValue = mods[modKey].mod;
+  player.activeMods.push(modValue);
+  console.log(player.activeMods);
+  //console.log("Mod 3 clicked");
+}
+function clickedMod4(modKey) {
+  let modValue = mods[modKey].mod;
+  player.activeMods.push(modValue);
+  console.log(player.activeMods);
+  //console.log("Mod 4 clicked");
+}
+function clickedMod5(modKey) {
+  let modValue = mods[modKey].mod;
+  player.activeMods.push(modValue);
+
+  console.log(player.activeMods);
+  console.log("Mod 5 clicked");
+}
+function clickedMenu1() {
+  //console.log("Menu button 1 clicked");
+}
+function clickedMenu2() {
+  //console.log("Menu button 2 clicked");
+}
+function clickedMenu3() {
+  //console.log("Menu button 3 clicked");
+}
+
+function modRate() {}
+function drawAll() {
+  console.log("drawAll() called....");
+  document.getElementById("pointTotal").innerHTML = totalPts.toString();
 }
