d0bd88465eb4d7313d1f09ea8ffb6d004f765b48
DennisMWarner <warner.dennis.m@gmail.com>
2020-03-22 03:41:04 -0600

    js and html modified

--- game.js
+++ game.js
@@ -2,72 +2,125 @@
 let totalPts = 0;
 let activeModRate = 0;
 let basePoints = 1;
-let donkeys = 0;
 
 let player = {
   level: "",
   nextLevel: 0,
-  donkey: false,
-  activeMods: []
+  activeMods: [],
+  donkeys: 0,
+  carts: 0,
+  pickaxes: 0,
+  saddlebags: 0
 };
 
 let mods = {
-  moonPickaxe: { mod: 2, cost: 100, costMultiplier: 2 },
-  moonDonkey: { mod: 4, cost: 1000, costMultiplier: 2 },
-  moonSaddlebag: { mod: 3, cost: 200, costMultiplier: 2 },
-  moonWagon: { mod: 5, cost: 5000, costMultiplier: 2 },
-  moonCarrots: { mod: 1, cost: 200, costMultplier: 1 }
+  moonPickaxe: {
+    name: "Moon Pickaxe",
+    qty: 0,
+    next: 0,
+    mod: 2,
+    cost: 100,
+    costMultiplier: 2
+  },
+  moonDonkey: {
+    name: "Moon Donkey",
+    qty: 0,
+    next: 0,
+    mod: 4,
+    cost: 1000,
+    costMultiplier: 2
+  },
+  moonSaddlebag: {
+    name: "Moon Saddlebag",
+    qty: 0,
+    next: 0,
+    mod: 3,
+    cost: 200,
+    costMultiplier: 2
+  },
+  moonWagon: {
+    name: "Moon Cart",
+    qty: 0,
+    next: 0,
+    mod: 5,
+    cost: 5000,
+    costMultiplier: 2
+  },
+  moonCarrots: {
+    name: "Moon Carrots",
+    qty: 0,
+    next: 0,
+    mod: 1,
+    cost: 200,
+    costMultplier: 1
+  }
 };
+function drawModButtons() {
+  let modButtonTemplate = "";
 
+  for (let modKey in mods) {
+    if (mods.hasOwnProperty(modKey)) {
+      let mod = mods[modKey];
+
+      modButtonTemplate +=
+        /*html*/
+        `<div class="row text-light rounded text-center py-1 my-3 btn btn-secondary w-100" onClick="modClicked('${modKey}')">
+        <div class="col-12"><h2>${mod.name}</h2></div>
+        <div class="col-12 text-dark font-italic"><h3>Moon Cost: ${mod.cost}
+        </h3></div>
+      </div>`;
+    }
+  }
+  document.getElementById("modButtons").innerHTML = modButtonTemplate;
+}
+
+function drawModTotals() {
+  let modTotals = "";
+  for (let key in mods) {
+    if (mods.hasOwnProperty(key) && mods[key].qty != 0) {
+      let element = mods[key];
+      modTotals += /*html*/ `<div class="col-12 font-weight-light font-italic pl-4">${element.name}s:</div>
+     <div class="col-12 text-right pr-5">${element.qty}</div>`;
+    }
+  }
+  document.getElementById("activeMods").innerHTML = modTotals;
+}
 function carrotGenerator() {}
 function clickedMoon() {
-  //console.log("Moon clicked");
+  console.log("Moon clicked");
   totalPts++;
-  drawAll();
+  updateAll();
 }
-function clickedMod1(modKey) {
-  let modValue = mods[modKey].mod;
-  player.activeMods.push(modValue);
-  console.log(player.activeMods);
+function modClicked(modKey) {
+  let mod = mods[modKey];
+  player.activeMods.push(mod);
+  mod.qty++;
+  drawModTotals();
+
   //console.log("Mod 1 clicked");
 }
-function clickedMod2(modKey) {
-  let modValue = mods[modKey].mod;
-  player.activeMods.push(modValue);
-  console.log(player.activeMods);
-  //console.log("Mod 2 clicked");
-}
-function clickedMod3(modKey) {
-  let modValue = mods[modKey].mod;
-  player.activeMods.push(modValue);
-  console.log(player.activeMods);
-  //console.log("Mod 3 clicked");
-}
-function clickedMod4(modKey) {
-  let modValue = mods[modKey].mod;
-  player.activeMods.push(modValue);
-  console.log(player.activeMods);
-  //console.log("Mod 4 clicked");
-}
-function clickedMod5(modKey) {
-  let modValue = mods[modKey].mod;
-  player.activeMods.push(modValue);
 
+function updateAll() {
+  player.activeMods.forEach(element => {
+    totalPts += element.mod;
+  });
   console.log(player.activeMods);
-  console.log("Mod 5 clicked");
-}
-function clickedMenu1() {
-  //console.log("Menu button 1 clicked");
-}
-function clickedMenu2() {
-  //console.log("Menu button 2 clicked");
-}
-function clickedMenu3() {
-  //console.log("Menu button 3 clicked");
-}
-
-function modRate() {}
-function drawAll() {
-  console.log("drawAll() called....");
   document.getElementById("pointTotal").innerHTML = totalPts.toString();
+  drawModTotals();
 }
+
+function reset() {
+  player.activeMods.splice(0);
+  totalPts = 0;
+  for (const key in mods) {
+    if (mods.hasOwnProperty(key)) {
+      const element = mods[key];
+      element.qty = 0;
+    }
+  }
+  drawModTotals();
+  drawModButtons();
+  updateAll();
+}
+drawModButtons();
+drawModTotals();
